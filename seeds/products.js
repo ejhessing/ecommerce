8a4a9a393f9e82259a7662d70331a0250e160a9a
Erwin Hessing <ejhessing@Erwins-MacBook-Pro.local>
2016-08-11 07:33:16 +1200

    Moved later seeds

--- seeds/products.js
+++ seeds/products.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('products').insert({id: 301, name: 't-shirt blue', price: 14.50, image: "http://bsccongress.com/im4/blue-shirt-clip-art.png"}),
-        knex('products').insert({id: 302, name: 't-shirt red', price: 24.50, image: "http://www.clker.com/cliparts/8/4/i/v/6/X/red-t-shirt-hi.png"}),
-        knex('products').insert({id: 303, name: 't-shirt green', price: 34.50, image: "http://www.clker.com/cliparts/m/O/n/c/U/O/green-t-shirt.svg"})
+        knex('products').insert({id: 301, name: 't-shirt blue', price: 14, image: "http://bsccongress.com/im4/blue-shirt-clip-art.png"}),
+        knex('products').insert({id: 302, name: 't-shirt red', price: 24, image: "http://www.clker.com/cliparts/8/4/i/v/6/X/red-t-shirt-hi.png"}),
+        knex('products').insert({id: 303, name: 't-shirt green', price: 34, image: "http://www.clker.com/cliparts/m/O/n/c/U/O/green-t-shirt.svg"})
       ]);
     });
 };
